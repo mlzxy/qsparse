ca5569327db9b0f88c2e89da5efd9fb7b5b9e9b7
Xinyu Zhang <xiz368@eng.ucsd.edu>
2021-10-28 20:06:14 +0800

    add docstring to qsparse.imitation

--- qsparse/imitation.py
+++ qsparse/imitation.py
@@ -1,10 +1,41 @@
-from typing import Type
+from typing import Callable
 
 import torch
 import torch.nn as nn
 
 
-def imitate(human: nn.Module, name: str, thing: Type, bias_thing: Type = None):
+def imitate(
+    human: nn.Module, name: str, thing: Callable, bias_thing: Callable = None
+) -> nn.Module:
+    """Patch the class of the input module, in order to in-place transform the
+    weight / bias attributes of the input module.
+
+    Suppose we have an input module `mod` with a weight attribute, and `mod.weight` returns `tensor([1, 2])`.
+
+    ```python
+    mod2 = imitate(mod, "add1", lambda x: x + 1)
+    ```
+
+    Then `mod2` will behave identically to `mod`, except that `mod2.weight` will return `tensor([2, 3])`.
+    This mechanism enables us to have a much simpler API design than existing other prune/quantize libraries.
+
+    The only downside here is the returned module is not pickleable because python pickle doesn't support closure.
+    We believe it is a minor issue because
+        1. Only the state dict is pickled for most scenarios.
+        2. We could use cloudpickle like `torch.save(module, path, pickle_module=cloudpickle)`, which supports closures.
+
+    _Names of the arguments are inspired from the movie `The Thing`._
+
+    Args:
+        human (nn.Module): input module
+        name (str): name of the transform
+        thing (Callable): function that transforms the weight attribute
+        bias_thing (Callable, optional): function that transforms the bias attribute. Defaults to None.
+
+    Returns:
+        nn.Module: output module whose weight / bias attributes are transformed
+    """
+
     InputClass = human.__class__
 
     def get_prev_weight(self: nn.Module):
@@ -23,8 +54,6 @@ def imitate(human: nn.Module, name: str, thing: Type, bias_thing: Type = None):
 
     if bias_thing is not None:
         setattr(human, name + "_bias", bias_thing)
-
-    # python pickle doesn't support closure, so to use torch.save/load,  we need to use like `torch.save(data, path, pickle_module=cloudpickle)`
 
     class Imitation(InputClass):
         @property
