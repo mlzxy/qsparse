912dfcf8e41e58a9afd61e643651db77ee5de733
Xinyu Zhang <xiz368@eng.ucsd.edu>
2021-10-29 17:30:40 +0800

    add tests for linear_quantize_callback for scalar/vector quantization

--- tests/test_quantize.py
+++ tests/test_quantize.py
@@ -1,3 +1,6 @@
+import numpy as np
+import torch
+
 from qsparse import linear_quantize_callback
 
 
@@ -10,6 +13,24 @@ def test_weight():
 
 
 def test_callback():
-    # vector
-    # channel index
-    pass
+    # scalar quantization
+    data = torch.rand(10, 3, 32, 32)
+    qdata = linear_quantize_callback(data, bits=8, decimal=7)
+    assert np.allclose(data.numpy(), qdata.numpy(), atol=1 / 2 ** 7)
+
+    # vector quantization
+    for i in range(1, 4):
+        decimals = torch.randint(1, 7, (data.shape[i],))
+        qdata = linear_quantize_callback(
+            data, bits=8, decimal=decimals, channel_index=i
+        )
+        for j in range(data.shape[i]):
+            indices = [
+                slice(None),
+            ] * 4
+            indices[i] = j
+            assert np.allclose(
+                data[indices].numpy(),
+                qdata[indices].numpy(),
+                atol=1 / 2 ** decimals[j].item(),
+            )
