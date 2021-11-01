d9d4b10f083699ed9e03e371317d2a0438fe11e7
Xinyu Zhang <xiz368@eng.ucsd.edu>
2021-11-01 13:31:46 +0800

    add readthedocs compatible post doc build scripts

---
+++ docs/setup.py
@@ -0,0 +1,16 @@
+"""
+This file is used to execute pre-build tasks for readthedocs compilation
+"""
+
+import subprocess
+import shlex
+
+
+def shell(cmd):
+    subprocess.call(shlex.split(cmd))
+
+
+if __name__ == "__main__":
+    shell("mkdir -p site")
+    shell("cp ./LICENSE.txt site/")
+    shell("cp -r ./docs site/")
