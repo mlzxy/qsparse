3fa9c46e50caf11a2a33629bf79657665dad15d9
Xinyu Zhang <xiz368@eng.ucsd.edu>
2021-11-20 20:55:10 +0800

    fully implement the `convert` function, needs further testing

--- qsparse/convert.py
+++ qsparse/convert.py
@@ -1,25 +1,26 @@
 import copy
-from typing import Optional, Sequence, Type, Union
+from typing import Mapping, Optional, Sequence, Tuple, Type, Union
 
-import torch
 import torch.nn as nn
+from torch.nn.modules.container import Sequential
 
 from qsparse.quantize import QuantizeLayer, quantize
 from qsparse.sparse import PruneLayer, prune
 from qsparse.util import auto_name_prune_quantize_layers
 
 
-def convert(
+def convert(  # noqa: C901
     model: nn.Module,
     operator: Union[PruneLayer, QuantizeLayer],
     inplace: bool = False,
     weight_layers: Sequence[Type[nn.Module]] = [nn.Conv2d],
     activation_layers: Sequence[Type[nn.Module]] = [nn.BatchNorm2d],
     input: bool = False,
-    output: bool = False,
     log: bool = False,
-    excluded_weight_layer_indexes: Sequence[int] = [],
-    excluded_activation_layer_indexes: Sequence[int] = [],
+    excluded_weight_layer_indexes: Sequence[Tuple[Type[nn.Module], Sequence[int]]] = [],
+    excluded_activation_layer_indexes: Sequence[
+        Tuple[Type[nn.Module], Sequence[int]]
+    ] = [],
 ) -> nn.Module:
     """Automatically convert a model to a new model with its weights and
     activations transformed by the operator, e.g. [prune][qsparse.sparse.prune] or [quantize][qsparse.quantize.quantize].
@@ -31,10 +32,9 @@ def convert(
         weight_layers (Sequence[Type[nn.Module]], optional): which layers to apply operator to transform weights. Defaults to [nn.Conv2d].
         activation_layers (Sequence[Type[nn.Module]], optional): which layers to apply operator to transform output activations. Defaults to [nn.BatchNorm2d].
         input (bool, optional): whether apply operator to input. Defaults to False.
-        output (bool, optional): whether apply operator to output. Defaults to False.
         log (bool, optional): whether print the conversion log. Defaults to False.
-        excluded_weight_layer_indexes (Sequence[int], optional): indexes of layers excluded in weight transformations from conversion. Defaults to [].
-        excluded_activation_layer_indexes (Sequence[int], optional): indexes of layers excluded in activation transformations from conversion. Defaults to [].
+        excluded_weight_layer_indexes (Sequence[Tuple[Type[nn.Module], Sequential[int]]], optional): indexes of layers excluded in weight transformations from conversion. Defaults to [].
+        excluded_activation_layer_indexes (Sequence[Tuple[Type[nn.Module], Sequential[int]]], optional): indexes of layers excluded in activation transformations from conversion. Defaults to [].
 
     Returns:
         nn.Module: converted module
@@ -46,6 +46,12 @@ def convert(
     def _print(msg):
         if log:
             print(msg)
+
+    def mstr(m) -> str:
+        if isinstance(m, nn.Module):
+            return m.__class__.__name__
+        else:
+            return m.__name__
 
     def apply_operator(layer: Optional[nn.Module] = None) -> nn.Module:
         if layer is not None:
@@ -59,19 +65,75 @@ def convert(
     if not inplace:
         model = copy.deepcopy(model)
 
-    weight_counter = {str(c): 0 for c in weight_layers}
-    activation_counter = {str(c): 0 for c in activation_layers}
+    def count_occurrence(
+        mod: nn.Module, layer_types: Sequence[Type[nn.Module]]
+    ) -> Mapping[str, int]:
+        def _count_occurrence(m: nn.Module, layer_type):
+            total = 0
+            for _, layer in m.named_children():
+                if not isinstance(layer, nn.Sequential):
+                    if mstr(layer) == mstr(layer_type):
+                        total += 1
+                else:
+                    total += _count_occurrence(layer)
+            return total
 
-    def _convert(mod: nn.Module) -> nn.Module:
+        return {mstr(l): _count_occurrence(mod, l) for l in layer_types}
+
+    def excluded_layer_indexes_to_dict(
+        layer_indexes, layers_count
+    ) -> Mapping[str, Sequence[int]]:
+        return {
+            mstr(cls): [l if l >= 0 else (l + layers_count[mstr(cls)]) for l in indexes]
+            for cls, indexes in layer_indexes
+        }
+
+    weight_counter = {mstr(c): 0 for c in weight_layers}
+    weight_total_layers_counts = count_occurrence(
+        model, [a for a, _ in excluded_weight_layer_indexes]
+    )
+    excluded_weight_layer_indexes = excluded_layer_indexes_to_dict(
+        excluded_weight_layer_indexes, weight_total_layers_counts
+    )
+
+    activation_counter = {mstr(c): 0 for c in activation_layers}
+    activation_total_layers_counts = count_occurrence(
+        model, [a for a, _ in excluded_activation_layer_indexes]
+    )
+    excluded_activation_layer_indexes = excluded_layer_indexes_to_dict(
+        excluded_activation_layer_indexes, activation_total_layers_counts
+    )
+
+    operator_name = str(operator).replace("(", "").replace(")", "")
+
+    def _convert(mod: nn.Module, scope: str = "") -> nn.Module:
         reassign = {}
         for name, m in mod.named_children():
             origin_m = m
             if not isinstance(m, nn.Sequential):
-                if str(m.__class__) in weight_counter:
-                    m = apply_operator(m)
+                if mstr(m) in weight_counter:
+                    if (
+                        weight_counter[mstr(m)]
+                        not in excluded_weight_layer_indexes[mstr(m)]
+                    ):
+                        _print(f"Apply {operator_name} on the {scope}.{name} weight")
+                        m = apply_operator(m)
+                    else:
+                        _print(f"Exclude {scope}.{name} weight")
+                    weight_counter[mstr(m)] += 1
 
-                if str(m.__class__) in activation_counter:
-                    m = [m, apply_operator()]
+                if mstr(m) in activation_counter:
+                    if (
+                        activation_counter[mstr(m)]
+                        not in excluded_activation_layer_indexes[mstr(m)]
+                    ):
+                        _print(
+                            f"Apply {operator_name} on the {scope}.{name} activation"
+                        )
+                        m = [m, apply_operator()]
+                    else:
+                        _print(f"Exclude {scope}.{name} activation")
+                    activation_counter[mstr(m)] += 1
 
                 if origin_m is not m:
                     if isinstance(m, list):
@@ -79,11 +141,14 @@ def convert(
                     else:
                         reassign[name] = m
             else:
-                _convert(m)
+                _convert(m, f"{scope}.{name}.")
         for key, value in reassign.items():
             mod._modules[key] = value
         return mod
 
     model = _convert(model)
+    if input:
+        model = nn.Sequential(apply_operator(), model)
+
     model = auto_name_prune_quantize_layers(model)
     return model
